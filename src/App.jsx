afb78419b1cf463d00ba11037f8ef560c42ea5a4
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2023-11-18 23:00:44 -0600

    Add score elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar';
+import Score from './components/Score';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Score />
     </>
   );
 }
---
+++ src/components/Score.jsx
@@ -0,0 +1,8 @@
+export default function Score(props) {
+  return (
+    <div className='main--score-container'>
+      <h3 className='main--current-score'>Score:</h3>
+      <h3 className='main--high-score'>Best:</h3>
+    </div>
+  );
+}
