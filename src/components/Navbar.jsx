c90128c453e9bd3bf379d71d248508c0933344bf
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2023-11-18 22:57:58 -0600

    Add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+      <img
+        className='navbar--soccer-image'
+        src='src/assets/soccer-ball.png'
+        alt=''
+      />
+      <h2 className='navbar--title'>Soccer Memory Card</h2>
+    </nav>
+  );
+}
